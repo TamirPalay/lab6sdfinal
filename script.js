a4dd63dd9a58df56925045bf4504193ef3e4a2c3
TamirPalay <typalay@gmail.com>
2024-04-10 12:38:50 +0200

    fixed spelling error

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const carList = document.getElementById('carList');
     cars = [];
     loadCarsBtn.addEventListener('click', () => {
-        fetch('/api/cars')
+        fetch('/api/getAllCars')
             .then(response => response.json())
             .then(data => {
                 cars = data;
